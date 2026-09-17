90ab0f0cf8c9ade13ac7a4fa6d1cc32e2d24a0d3
Sourabh-Singh-Chuphal <sourabhchufal4141@email.com>
2026-09-17 15:54:29 +0530

    Initial commit

---
+++ Data_types.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void greet()
+{
+    printf("Hello, welcome!\n");
+}
+
+int main() {
+    int var = 22;
+
+    printf("var = %d \n", var);
+    
+    char ch = 'A';
+    
+    printf("ch = %c \n", ch);
+    greet();
+    return 0;
+}
+
+
+
+
+    
---
+++ Data_types.py
@@ -0,0 +1,19 @@
+#Data types
+# primitive data types -> int, float, char, 
+a = 5
+b = 90.001
+name = 'Sourabh'
+name2 = 'a'
+print(a)
+print(b)
+print(name)
+print(name2)
+print(type(a))
+print(type(b))
+print(type(name))
+print(type(name2))
+
+def greet():
+    print("Hello, king! Sourabh is here to help you with your DSA journey!")
+
+greet()
